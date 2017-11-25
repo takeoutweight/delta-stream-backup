224ae347cbc87d812e71574fccbb099e317a2c22
Nathan Sorenson <nathan.d.sorenson@gmail.com>
2017-11-25 00:16:29 -0800

    Experiments specializing all_ - not sure it can be done

--- src/DBHelpers.hs
+++ src/DBHelpers.hs
@@ -4,7 +4,9 @@
 module DBHelpers where
 
 import qualified Control.Monad.IO.Class as CM
+import qualified Database.Beam as DB
 import qualified Database.Beam.Backend.SQL as DBS
+import qualified Database.Beam.Backend.SQL.SQL92 as SQL92
 import qualified Database.Beam.Query as DBQ
 import qualified Database.Beam.Query.Internal as DBQI
 import qualified Database.Beam.Sqlite as DBS -- Doesn't like my selectOne type
@@ -51,6 +53,7 @@ selectOne conn query =
 
 {-
 -- Database.Beam.Sqlite.Syntax specializes IsSql92SelectTableSyntax SqliteSelectTableSyntax
+-- This can't actually be explicitly typed because SqliteProjectionSyntax isn't exported
 all_
   :: (Database db,
       Table table) =>
@@ -64,3 +67,18 @@ all_
                           SqliteSelectSyntax))
                     s))
 -}
+
+myAll_
+  :: (DB.Database db,
+      SQL92.IsSql92SelectSyntax select,
+      DB.Table table) =>
+     DB.DatabaseEntity be db (DB.TableEntity table)
+     -> DBQ.Q select -- DBSS.SqliteCommandSyntax 
+          db
+          s
+          (table (DBQI.QExpr
+                    (SQL92.Sql92SelectTableExpressionSyntax
+                       (SQL92.Sql92SelectSelectTableSyntax
+                         select)) -- DBSS.SqliteSelectSyntax
+                    s))
+myAll_ = DBQ.all_
