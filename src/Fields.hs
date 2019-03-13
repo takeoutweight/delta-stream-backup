b2d739c81a8674717988ffeaac348876d0cd487f
Nathan Sorenson <nathan.d.sorenson@gmail.com>
2019-03-12 17:29:29 -0700

    IDK messing with .ghci

--- src/Fields.hs
+++ src/Fields.hs
@@ -60,14 +60,14 @@ infixr 5 &:
 
 -- our fields
 
-newtype DBPath = DBPath String deriving (Show, Generic)
+newtype DBPath = DBPath String deriving (Show, Read, Generic)
 instance Wrapped DBPath
 
 -- | Location is a place where a tree of files canbe located (i.e. not the
 -- location of a particular file). It is a hostname and a path to the root, like
 -- Nathans-MacBook-Pro-2.local/Users/nathan/backup/loc1/. Prepended w/o host to
 -- RelativePath for an absolute path.
-newtype Location = Location Text deriving (Show, Generic, Eq, Ord)
+newtype Location = Location Text deriving (Show, Read, Generic, Eq, Ord)
 instance Wrapped Location
 
 -- | AbsPath is the entire real filesystem path (path to location root in
@@ -77,59 +77,59 @@ newtype AbsPath = AbsPath FilePath deriving (Show, Generic)
 instance Wrapped AbsPath
 
 -- | Because conversion of paths to text can fail.
-newtype AbsPathText = AbsPathText Text deriving (Show, Generic, Eq, Ord)
+newtype AbsPathText = AbsPathText Text deriving (Show, Read, Generic, Eq, Ord)
 instance Wrapped AbsPathText
 
 -- | Relative to the root of the location
-newtype RelativePathText = RelativePathText Text deriving (Show, Generic)
+newtype RelativePathText = RelativePathText Text deriving (Show, Read, Generic)
 instance Wrapped RelativePathText
 
-newtype Filename = Filename Text deriving (Show, Generic)
+newtype Filename = Filename Text deriving (Show, Read, Generic)
 instance Wrapped Filename
 
 newtype Rechecksum = Rechecksum (Maybe UTCTime -> Maybe UTCTime -> Bool) deriving (Generic)
 instance Wrapped Rechecksum
 
 -- | Null CheckTime means we know the expected hash but we've never checked
-newtype CheckTime = CheckTime (Maybe UTCTime) deriving (Show, Generic)
+newtype CheckTime = CheckTime (Maybe UTCTime) deriving (Show, Read, Generic)
 instance Wrapped CheckTime
 
-newtype ModTime = ModTime UTCTime deriving (Show, Generic, Eq)
+newtype ModTime = ModTime UTCTime deriving (Show, Read, Generic, Eq)
 instance Wrapped ModTime
 
-newtype FileSize = FileSize Int deriving (Show, Generic, Eq)
+newtype FileSize = FileSize Int deriving (Show, Read, Generic, Eq)
 instance Wrapped FileSize
 
-newtype Checksum = Checksum Text deriving (Show, Generic, Eq)
+newtype Checksum = Checksum Text deriving (Show, Read, Generic, Eq)
 instance Wrapped Checksum
 
-newtype FileInfoIdText = FileInfoIdText Text deriving (Show, Generic)
+newtype FileInfoIdText = FileInfoIdText Text deriving (Show, Read, Generic)
 instance Wrapped FileInfoIdText
 
-newtype EventNumber = EventNumber Int deriving (Show, Generic)
+newtype EventNumber = EventNumber Int deriving (Show, Read, Generic)
 instance Wrapped EventNumber
 
-newtype Deleted = Deleted Bool deriving (Show, Generic)
+newtype Deleted = Deleted Bool deriving (Show, Read, Generic)
 instance Wrapped Deleted
 
 -- | Where the text is the key id used. This is only if the SYSTEM is handling the encryption. It won't detect files that happen to be encrypted on their own.
-data IsEncrypted = Encrypted Text | Unencrypted deriving (Show, Generic, Eq)
+data IsEncrypted = Encrypted Text | Unencrypted deriving (Show, Read, Generic, Eq)
 
-newtype FileStateIdF = FileStateIdF Int  deriving (Show, Generic)
+newtype FileStateIdF = FileStateIdF Int  deriving (Show, Read, Generic)
 instance Wrapped FileStateIdF
 
 -- | Each location has its own sequence counter
-newtype SequenceNumber = SequenceNumber Int deriving (Show, Generic)
+newtype SequenceNumber = SequenceNumber Int deriving (Show, Read, Generic)
 instance Wrapped SequenceNumber
 
 -- | Did this entry represent a mirror from somewhere else? Or straight-from-the-filesystem?
-data Provenance = Mirrored Int | Ingested deriving (Show, Generic)
+data Provenance = Mirrored Int | Ingested deriving (Show, Read, Generic)
 
 -- | NonCanonical means this file/hash is not meant to be propagated. It possibly represents corrupted data.
-data Canonical = NonCanonical | Canonical deriving (Show, Generic)
+data Canonical = NonCanonical | Canonical deriving (Show, Read, Generic)
 
 -- | Actual means the record reflects the most current understanding of the real contents of the filesystem.
-data Actual = Historical | Actual  deriving (Show, Generic)
+data Actual = Historical | Actual  deriving (Show, Read, Generic)
 
 type HasFileDetails rs = (Has ModTime rs, Has FileSize rs, Has Checksum rs, Has IsEncrypted rs)
 
